72128703b0689db7d4c6eeba520e186a664c4d28
D3364 <D3364@163.com>
2025-11-21 10:50:46 +0800

    代码提交

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import LandingDesktop from './components/LandingDesktop';
+
+// Unified entry: reuse the desktop experience on all viewports for consistent visuals.
+const App = () => <LandingDesktop />;
+
+export default App;
